36be92b2e51680ab74f4e05a3454720d595f4c92
Jose Luis Salas <josacar@gmail.com>
2014-10-08 10:41:59 +0000

    Add SQS listener in adapter

--- lib/lita/adapters/slack.rb
+++ lib/lita/adapters/slack.rb
@@ -3,73 +3,81 @@ require 'faraday'
 require 'json'
 
 module Lita
-	module Adapters
-		class Slack < Adapter
-			# Required Lita config keys (via lita_config.rb)
-			require_configs :incoming_token, :team_domain
+  module Adapters
+    class Slack < Adapter
+      # Required Lita config keys (via lita_config.rb)
+      require_configs :incoming_token, :team_domain
 
-			# Adapter main run loop
-			def run
-				log.debug 'Slack::run started'
-				sleep
-				rescue Interrupt
-				shut_down
-			end
+      # Adapter main run loop
+      def run
+        listen_to_sqs
+        log.debug 'Slack::run started'
+        sleep
+      rescue Interrupt
+        shut_down
+      end
 
-			def send_messages(target, strings)
-				log.debug 'Slack::send_messages started'
-				status = http_post prepare_payload(target, strings)
-				log.error "Slack::send_messages failed to send (#{status})" if status != 200
-				log.debug 'Slack::send_messages ending'
-			end
+      def send_messages(target, strings)
+        log.debug 'Slack::send_messages started'
+        status = http_post prepare_payload(target, strings)
+        log.error "Slack::send_messages failed to send (#{status})" if status != 200
+        log.debug 'Slack::send_messages ending'
+      end
 
-			def set_topic(target, topic)
-				# Slack currently provides no method
-				log.info 'Slack::set_topic no implementation'
-			end
+      def set_topic(target, topic)
+        # Slack currently provides no method
+        log.info 'Slack::set_topic no implementation'
+      end
 
-			def shut_down
-			end
+      def shut_down
+      end
 
       private
 
-			def prepare_payload(target, strings)
-				if not defined?(target.room)
-					channel_id = nil
-					log.warn "Slack::prepare_payload proceeding without channel designation"
-				else
-					channel_id = target.room
-				end
-				payload = {'channel' => channel_id, 'username' => username}
-				payload['text'] = strings.join('\n')
-				if add_mention? and defined?(target.user.id)
-					payload['text'] = payload['text'].prepend("<@#{target.user.id}> ")
-				end
-				return payload
-			end
+      def listen_to_sqs
+        return unless config.sqs_queue_name
+        require 'lita/adapters/slack/sqs_subscriber'
+        SqsSubscriber.listen(robot)
+        log.debug 'Listening to SQS messages'
+      end
 
-			def http_post(payload)
-				res = Faraday.post do |req|
-					log.debug "Slack::http_post sending payload to #{incoming_url}; length: #{payload.to_json.size}"
-					req.url config.incoming_url, :token => config.incoming_token
-					req.headers['Content-Type'] = 'application/json'
-					req.body = payload.to_json
-				end
-				log.info "Slack::http_post sent payload with response status #{res.status}"
-				log.debug "Slack::http_post response body: #{res.body}"
-				return res.status
-			end
+      def prepare_payload(target, strings)
+        if not defined?(target.room)
+          channel_id = nil
+          log.warn "Slack::prepare_payload proceeding without channel designation"
+        else
+          channel_id = target.room
+        end
+        payload = {'channel' => channel_id, 'username' => username}
+        payload['text'] = strings.join('\n')
+        if add_mention? and defined?(target.user.id)
+          payload['text'] = payload['text'].prepend("<@#{target.user.id}> ")
+        end
+        payload
+      end
 
-			#
-			# Accessor shortcuts
-			#
-			def config
-				Lita.config.adapter
-			end
+      def http_post(payload)
+        res = Faraday.post do |req|
+          log.debug "Slack::http_post sending payload to #{incoming_url}; length: #{payload.to_json.size}"
+          req.url incoming_url, :token => config.incoming_token
+          req.headers['Content-Type'] = 'application/json'
+          req.body = payload.to_json
+        end
+        log.info "Slack::http_post sent payload with response status #{res.status}"
+        log.debug "Slack::http_post response body: #{res.body}"
+        res.status
+      end
 
-			def log
-				Lita.logger
-			end
+      #
+      # Accessor shortcuts
+      #
+      def config
+        Lita.config.adapter
+      end
+
+      def log
+        Lita.logger
+      end
 
       def incoming_url
         config.incoming_url ||
@@ -83,9 +91,9 @@ module Lita
       def add_mention?
         config.add_mention
       end
-		end
+    end
 
-		# Register Slack adapter to Lita
-		Lita.register_adapter(:slack, Slack)
-	end
+    # Register Slack adapter to Lita
+    Lita.register_adapter(:slack, Slack)
+  end
 end
--- spec/lita/adapters/slack_spec.rb
+++ spec/lita/adapters/slack_spec.rb
@@ -1,53 +1,53 @@
 require "spec_helper"
 
 describe Lita::Adapters::Slack, lita: true do
-	before do
-		Lita.configure do |config|
-			config.adapter.incoming_token = 'aN1NvAlIdDuMmYt0k3n'
-			config.adapter.team_domain = 'example'
-			config.adapter.username = 'lita'
-			config.adapter.add_mention = true
-		end
-	end
+  before do
+    Lita.configure do |config|
+      config.adapter.incoming_token = 'aN1NvAlIdDuMmYt0k3n'
+      config.adapter.team_domain = 'example'
+      config.adapter.username = 'lita'
+      config.adapter.add_mention = true
+    end
+  end
 
-	subject { described_class.new(robot) }
-	let(:robot) { double("Lita::Robot") }
+  subject { described_class.new(robot) }
+  let(:robot) { double("Lita::Robot") }
 
-	it "registers with Lita" do
-		expect(Lita.adapters[:slack]).to eql(described_class)
-	end
+  it "registers with Lita" do
+    expect(Lita.adapters[:slack]).to eql(described_class)
+  end
 
-	it "fails without valid config: incoming_token and team_domain" do
-		Lita.clear_config
-		expect(Lita.logger).to receive(:fatal).with(/incoming_token, team_domain/)
-		expect { subject }.to raise_error(SystemExit)
-	end
+  it "fails without valid config: incoming_token and team_domain" do
+    Lita.clear_config
+    expect(Lita.logger).to receive(:fatal).with(/incoming_token, team_domain/)
+    expect { subject }.to raise_error(SystemExit)
+  end
 
-	describe "#send_messages" do
-		it "sends JSON payload via HTTP POST to Slack channel" do
-			target = double("Lita::Source", room: "CR00M1D")
-			payload = {'channel' => target.room, 'username' => Lita.config.adapter.username, 'text' => 'Hello!'}
-			expect(subject).to receive(:http_post).with(payload)
-			subject.send_messages(target, ["Hello!"])
-		end
+  describe "#send_messages" do
+    it "sends JSON payload via HTTP POST to Slack channel" do
+      target = double("Lita::Source", room: "CR00M1D")
+      payload = {'channel' => target.room, 'username' => Lita.config.adapter.username, 'text' => 'Hello!'}
+      expect(subject).to receive(:http_post).with(payload)
+      subject.send_messages(target, ["Hello!"])
+    end
 
-		it "sends message with mention if user info is provided" do
-			user = double("Lita::User", id: "UM3NT10N")
-			target = double("Lita::Source", room: "CR00M1D", user: user)
-			text = "<@#{user.id}> Hello!"
-			payload = {'channel' => target.room, 'username' => Lita.config.adapter.username, 'text' => text}
-			expect(subject).to receive(:http_post).with(payload)
-			subject.send_messages(target, ["Hello!"])
-		end
+    it "sends message with mention if user info is provided" do
+      user = double("Lita::User", id: "UM3NT10N")
+      target = double("Lita::Source", room: "CR00M1D", user: user)
+      text = "<@#{user.id}> Hello!"
+      payload = {'channel' => target.room, 'username' => Lita.config.adapter.username, 'text' => text}
+      expect(subject).to receive(:http_post).with(payload)
+      subject.send_messages(target, ["Hello!"])
+    end
 
-		it "proceeds but logs WARN when directed to an user without channel(room) info" do
-			user = double("Lita::User", id: "UM3NT10N")
-			target = double("Lita::Source", user: user)
-			text = "<@#{user.id}> Hello!"
-			payload = {'channel' => nil, 'username' => Lita.config.adapter.username, 'text' => text}
-			expect(subject).to receive(:http_post).with(payload)
-			expect(Lita.logger).to receive(:warn).with(/without channel/)
-			subject.send_messages(target, ["Hello!"])
-		end
-	end
+    it "proceeds but logs WARN when directed to an user without channel(room) info" do
+      user = double("Lita::User", id: "UM3NT10N")
+      target = double("Lita::Source", user: user)
+      text = "<@#{user.id}> Hello!"
+      payload = {'channel' => nil, 'username' => Lita.config.adapter.username, 'text' => text}
+      expect(subject).to receive(:http_post).with(payload)
+      expect(Lita.logger).to receive(:warn).with(/without channel/)
+      subject.send_messages(target, ["Hello!"])
+    end
+  end
 end
