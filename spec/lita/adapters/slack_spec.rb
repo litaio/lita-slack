a0d7f50b25170ca5891bb3f44a4f741b989ffb6e
Jimmy Cuadra <jimmy@jimmycuadra.com>
2014-12-08 15:48:41 -0800

    Reorganize test code.

--- spec/lita/adapters/slack_spec.rb
+++ spec/lita/adapters/slack_spec.rb
@@ -1,14 +1,15 @@
 require "spec_helper"
 
 describe Lita::Adapters::Slack, lita: true do
-  before do
-    registry.register_adapter(:slack, described_class)
-    registry.config.adapters.slack.token = 'abcd-1234567890-hWYd21AmMH2UHAkx29vb5c1Y'
-  end
-
   subject { described_class.new(robot) }
 
   let(:robot) { Lita::Robot.new(registry) }
+  let(:token) { 'abcd-1234567890-hWYd21AmMH2UHAkx29vb5c1Y' }
+
+  before do
+    registry.register_adapter(:slack, described_class)
+    registry.config.adapters.slack.token = token
+  end
 
   it "registers with Lita" do
     expect(Lita.adapters[:slack]).to eql(described_class)
