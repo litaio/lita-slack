3d28891b207e96f5437835f9b8214e50853d402b
Andreas Hubenthal <ibu2002@gmail.com>
2015-02-13 01:41:41 -0500

    Add tz user info

--- lib/lita/adapters/slack/slack_user.rb
+++ lib/lita/adapters/slack/slack_user.rb
@@ -7,7 +7,8 @@ module Lita
             new(
               user_data['id'],
               user_data['name'],
-              user_data['real_name']
+              user_data['real_name'],
+              user_data['tz']
             )
           end
 
@@ -19,11 +20,13 @@ module Lita
         attr_reader :id
         attr_reader :name
         attr_reader :real_name
+        attr_reader :tz
 
-        def initialize(id, name, real_name)
+        def initialize(id, name, real_name, tz)
           @id = id
           @name = name
           @real_name = real_name.to_s
+          @tz = tz
         end
       end
     end
