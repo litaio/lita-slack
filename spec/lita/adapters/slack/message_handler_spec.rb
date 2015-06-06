ecdad9a32c332300e47c2402db1bf7733c799c0d
Freibuis <freibuis@gmail.com>
2015-06-07 00:20:02 +1000

    Tests for formatting around skype links

--- spec/lita/adapters/slack/message_handler_spec.rb
+++ spec/lita/adapters/slack/message_handler_spec.rb
@@ -355,6 +355,24 @@ describe Lita::Adapters::Slack::MessageHandler, lita: true do
           end
         end
 
+        context "remove formatting around <skype> links" do
+          let(:data) do
+            {
+                "type"    => "message",
+                "channel" => "C2147483705",
+                "text"    => "foo <skype:echo123?call> bar",
+            }
+          end
+          it "removes formatting" do
+            expect(Lita::Message).to receive(:new).with(
+                                         robot,
+                                         "foo skype:echo123?call bar",
+                                         source
+                                     ).and_return(message)
+            subject.handle
+          end
+        end
+
 
       end
     end
