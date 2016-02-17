dce1cb73c9441556fae2db09b0e4fd71ac138c62
Riley Shott <rileyshott+git@gmail.com>
2016-02-17 10:34:42 -0800

    Added Slack profile back into metadata
    Fixes hmset issue in #57. The hmset method expects an even length
    Array. Any empty array in the metadata hash will be ejected when it's
    flattened and will result in an uneven Array.

--- lib/lita/adapters/slack/user_creator.rb
+++ lib/lita/adapters/slack/user_creator.rb
@@ -7,8 +7,7 @@ module Lita
           def create_user(slack_user, robot, robot_id)
             User.create(
               slack_user.id,
-              name: real_name(slack_user),
-              mention_name: slack_user.name
+              construct_metadata(slack_user)
             )
 
             update_robot(robot, slack_user) if slack_user.id == robot_id
@@ -28,6 +27,18 @@ module Lita
           def update_robot(robot, slack_user)
             robot.name = slack_user.real_name
             robot.mention_name = slack_user.name
+          end
+
+          def construct_metadata(slack_user)
+            metadata = slack_user.metadata['profile'] || {}
+            metadata.merge!({ name: real_name(slack_user), mention_name: slack_user.name })
+            compact metadata
+          end
+
+          # The future user.save will discard empty Arrays
+          # Redis requires an even number of arguments for hmset
+          def compact(hash)
+            hash.reject { |_, v| v.is_a?(Array) ? v.compact.empty? : false }
           end
         end
       end
